2058441216447494a78d5a5e0ee2b64c5f4b5066
aisitez <admin@aisitez.com>
2026-05-13 14:45:26 +0530

    fix: updated sign-in links and added premium Microsoft login page

--- crm/www/mslogin.py
+++ crm/www/mslogin.py
@@ -2,7 +2,7 @@ import frappe
 
 no_cache = 1
 
-
 def get_context(context):
-	frappe.local.flags.redirect_location = "/login"
-	raise frappe.Redirect
+    context.title = "Sign Up with Microsoft | SentimentAI CRM"
+    # We don't redirect here anymore, we want to show the page.
+    pass
